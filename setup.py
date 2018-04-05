68777e349862daa0fb19d1bdd829cba951bb6447
Bernardo Silva <benny.stuff@gmail.com>
2018-04-05 17:05:24 +0100

    set package dependencies in setup.py

--- setup.py
+++ setup.py
@@ -6,27 +6,29 @@ long_description = 'Please see our GitHub README'
 if os.path.exists('README.md'):
     long_description = open('README.md').read()
 
-test_requirements = ['pytest>=2.8.0']
+test_requirements = []
+
 
 def getRequires():
-    deps = []
+    deps = ['jsonschema', 'behave']
     if sys.version_info < (2, 7):
         deps.append('unittest2')
     elif (3, 0) <= sys.version_info < (3, 2):
         deps.append('unittest2py3k')
     return deps
 
+
 setup(
     name='behave-testrail-reporter',
     version='0.0.1',
     author='Virtualstock',
-    author_email='benny.stuff@gmail.com',
+    author_email='development.team@virtualstock.co.uk',
     url='https://github.com/virtualstock/behave-testrail-reporter/',
-    packages=find_packages(exclude=["temp*.py", "register.py", "test"]),
+    packages=find_packages(exclude=['temp*.py', 'register.py', 'test']),
     include_package_data=True,
     license='MIT',
     description='Behave library to integrate with Testrail API',
-    keywords= ['Behave', 'Testrail', 'API', 'Test', 'BDD'],
+    keywords=['Behave', 'Testrail', 'API', 'Test', 'BDD'],
     long_description=long_description,
     install_requires=getRequires(),
     classifiers=[
