13f0effce2318a6ca0b2cbe434f3d7fa1f4edcbc
Bernardo Silva <benny.stuff@gmail.com>
2018-04-05 15:11:52 +0100

    add some report output at end of tests and map more test status

--- behave_testrail_reporter/__init__.py
+++ behave_testrail_reporter/__init__.py
@@ -3,9 +3,38 @@ import os
 import yaml
 import sys
 
-import behave
 from behave.reporter.base import Reporter
+from behave.model import ScenarioOutline
+from behave.model_core import Status
+
 from .api import APIClient
+
+# -- DISABLED: optional_steps = ('untested', 'undefined')
+optional_steps = (Status.untested,) # MAYBE: Status.undefined
+status_order = (Status.passed, Status.failed, Status.skipped,
+                Status.undefined, Status.untested)
+
+
+def format_summary(statement_type, summary):
+    parts = []
+    for status in status_order:
+        if status.name not in summary:
+            continue
+        counts = summary[status.name]
+        if status in optional_steps and counts == 0:
+            # -- SHOW-ONLY: For relevant counts, suppress: untested items, etc.
+            continue
+
+        if not parts:
+            # -- FIRST ITEM: Add statement_type to counter.
+            label = statement_type
+            if counts != 1:
+                label += 's'
+            part = u'%d %s %s' % (counts, label, status.name)
+        else:
+            part = u'%d %s' % (counts, status.name)
+        parts.append(part)
+    return ', '.join(parts)
 
 
 class TestrailReporter(Reporter):
@@ -14,14 +43,20 @@ class TestrailReporter(Reporter):
     STATUS_UNTESTED = 3
     STATUS_RETEST = 4
     STATUS_FAILED = 5
+    show_failed_cases = True
 
     CASE_TAG_PREFIX = 'testrail-C'
 
+    # map Behave to Testrail test result status
     STATUS_MAPS = {
         'passed': STATUS_PASSED,
         'failed': STATUS_FAILED,
         'skipped': STATUS_UNTESTED,
+        'undefined': STATUS_UNTESTED,
+        'executing': STATUS_UNTESTED,
+        'untested': STATUS_UNTESTED,
     }
+
 
     def __init__(self, branch_name):
         self.config = {}
@@ -36,14 +71,34 @@ class TestrailReporter(Reporter):
         self.testrail_cases = {}
         self._load_config()
         self.setup_test_run()
+        self.case_summary = {
+            Status.passed.name: 0, 
+            Status.failed.name: 0,
+            Status.skipped.name: 0, 
+            Status.untested.name: 0}
+        self.duration = 0.0
+        self.failed_cases = []
 
     def feature(self, feature):
-        for scenario in feature.scenarios:
-            self.process_scenario(scenario)
+        self.duration += feature.duration
+        for scenario in feature:
+            if isinstance(scenario, ScenarioOutline):
+                self.process_scenario_outline(scenario)
+            else:
+                self.process_scenario(scenario)
 
     def end(self):
-        print('---end of tests being called from reporter--')
-        # @todo print report of what happened in this reporter, e.g. the number os tests pushed to testrail successfully
+         # -- SHOW FAILED SCENARIOS (optional):
+        if self.show_failed_cases and self.failed_cases:
+            print("\nFailing test case pushes to Testrail:\n")
+            for case_id in self.failed_cases:
+                print(u"case_id:  %s\n" % (case_id))
+            print("\n")
+
+        # -- SHOW SUMMARY COUNTS:
+        print(format_summary('testrail test case', self.case_summary))
+        timings = (int(self.duration / 60.0), self.duration % 60)
+        print('Took %dm%02.3fs\n' % timings)
 
     def _load_config(self):
         with open('testrail.yml', 'r') as stream:
@@ -63,16 +118,17 @@ class TestrailReporter(Reporter):
                 id=project_config.get('id'),
                 name=project_config.get('name'),
                 suite_id=project_config.get('suite_id'),
-                allowed_branch_pattern=project_config.get('suite_id')
+                allowed_branch_pattern=project_config.get('allowed_branch_pattern')
             )
             self.projects.append(testrail_project)
 
-    def _can_generate_test_run_for_branch(self, project, branch_name):
+    def _can_generate_test_run_for_branch(self, testrail_project, branch_name):
         """
         This method contains the logic to decide if the branch_name is allowed to have test run and
         test results added to it.
         """
-        allowed_branch_names = r'' + str(project.allowed_branch_pattern)
+        allowed_branch_names = r'' + str(testrail_project.allowed_branch_pattern)
+        print('branch {} is {}'.format(branch_name, testrail_project.allowed_branch_pattern))
 
         return bool(re.match(allowed_branch_names, branch_name))
 
@@ -111,7 +167,6 @@ class TestrailReporter(Reporter):
         """
         Reports the test results for the given scenario to the testrail run.
         """
-        pass
         for tag in scenario.tags + scenario.feature.tags:
             if tag.startswith(TestrailReporter.CASE_TAG_PREFIX):
                 case_id = tag[len(TestrailReporter.CASE_TAG_PREFIX):]
@@ -121,28 +176,37 @@ class TestrailReporter(Reporter):
                         comment = '{}\n'.format(scenario.name)
                         comment += '\n'.join(
                             ['->  {} {} [{}]'.format(step.keyword, step.name, step.status) for step in scenario.steps])
-                        status = TestrailReporter.STATUS_MAPS[scenario.status]
+
+                        testrail_status = TestrailReporter.STATUS_MAPS[scenario.status]
+
                         # When adding a test result untested status is not allowed status
                         # @see http://docs.gurock.com/testrail-api2/reference-results#add_result
-                        if status is self.STATUS_UNTESTED:
-                            # @todo add this scenario as skipped for reporting purposes
+                        if testrail_status is self.STATUS_UNTESTED:
+                            self.case_summary[Status.skipped.name] += 1
                             continue
 
                         if not self._can_generate_test_run_for_branch(testrail_project, self.branch_name):
-                            # @todo add this case as not pushed because it does not have a valid branch name
+                            self.case_summary[Status.skipped.name] += 1
                             continue
 
                         was_test_result_added = self.testrail_client.create_result(
                             testrail_project.test_run['id'],
                             case_id,
-                            status=status,
+                            status=testrail_status,
                             comment=comment,
                             elapsed='%ds' % int(scenario.duration)
                         )
 
                         if was_test_result_added:
-                            # @todo add this case_id as pushed successfully to testrail
-                            print('test result added succesfully for test case:', case_id)
+                            self.case_summary[Status.passed.name] += 1
+                        else:
+                            self.failed_cases.append(case_id)
+                    else:
+                        self.case_summary[Status.untested.name] += 1
+
+    def process_scenario_outline(self, scenario_outline):
+        for scenario in scenario_outline.scenarios:
+            self.process_scenario(scenario)
 
 
 
