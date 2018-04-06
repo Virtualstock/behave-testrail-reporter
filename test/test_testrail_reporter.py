87273199b9c4cadb22902739614426d180364eb1
Bernardo Silva <benny.stuff@gmail.com>
2018-04-06 13:36:26 +0100

    use dict literals in tests since they are actually faster

--- test/test_testrail_reporter.py
+++ test/test_testrail_reporter.py
@@ -13,23 +13,23 @@ from behave_testrail_reporter import TestrailReporter
 
 class TestrailReporterTestCase(unittest.TestCase):
     def setUp(self):
-        data = dict(
-            base_url='https://test.testrail.net',
-            projects=[
-                dict(
-                    name='Testrail project name',
-                    id=1,
-                    suite_id=11,
-                    allowed_branch_pattern='*',
-                ),
-                dict(
-                    name='Testrail project name',
-                    id=2,
-                    suite_id=22,
-                    allowed_branch_pattern='*',
-                )
+        data = {
+            'base_url': 'https://test.testrail.net',
+            'projects': [
+                {
+                    'name': 'Testrail project name',
+                    'id': 1,
+                    'suite_id': 11,
+                    'allowed_branch_pattern': '*',
+                },
+                {
+                    'name': 'Testrail project name',
+                    'id': 2,
+                    'suite_id': 22,
+                    'allowed_branch_pattern': '*',
+                }
             ]
-        )
+        }
 
         with open('testrail.yml', 'w') as outfile:
             yaml.dump(data, outfile, default_flow_style=False)
