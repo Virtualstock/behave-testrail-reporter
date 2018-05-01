75d8b6f9551b6720ded60701efd05b15cf78dc9a
Bernardo Silva <benny.stuff@gmail.com>
2018-05-01 13:43:53 +0100

    Bump library version to 0.1.0

--- setup.py
+++ setup.py
@@ -11,7 +11,7 @@ test_requirements = ['coverage', 'flake8', 'mock', 'twine', 'codacy-coverage']
 
 setup(
     name='behave-testrail-reporter',
-    version='0.0.2',
+    version='0.1.0',
     author='Virtualstock',
     author_email='development.team@virtualstock.co.uk',
     url='https://github.com/virtualstock/behave-testrail-reporter/',
